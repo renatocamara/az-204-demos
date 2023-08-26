4445e517c3e20a1263a8c4817c6c103932321b1b
John Doe <john.doe@contoso.com>
2023-08-25 20:04:31 -0400

    adjusts

---
+++ M3/5-sdk/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Specialized;
+
+Console.WriteLine("Blob demo");
+
+var connectionString = "";
+
+var containerName = "demo";
+
+var container = new BlobContainerClient(connectionString, containerName);
+
+await container.CreateIfNotExistsAsync();
+
+Console.WriteLine("Container created");
+
+Console.WriteLine("Uploading block blob");
+{
+     var blobName = "blockBlob.txt";
+     var content = "Hello from my code!";
+
+     var bytes = Encoding.UTF8.GetBytes(content);
+     var stream = new MemoryStream(bytes);
+
+     await container.UploadBlobAsync(blobName, stream);
+
+     Console.WriteLine("Block blob uploaded");
+}
+
+Console.WriteLine("Uploading page blob");
+{
+    var blobName = "pageBlob.txt";
+    var content = "Hello from my code!";
+
+    var pageClient = container.GetPageBlobClient(blobName);
+
+    Console.WriteLine($"Page size: {pageClient.PageBlobPageBytes} bytes");
+
+    var page = new byte[pageClient.PageBlobPageBytes];
+
+    var bytes = Encoding.UTF8.GetBytes(content);
+
+    Array.Copy(bytes, page, bytes.Length);
+
+    await pageClient.CreateIfNotExistsAsync(pageClient.PageBlobPageBytes);
+
+    await pageClient.UploadPagesAsync(new MemoryStream(page), 0);    
+
+    Console.WriteLine("Page blob uploaded");
+}
+
+Console.WriteLine("Done!");
